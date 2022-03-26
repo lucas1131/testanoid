1af287ab0bfcac627d4cf80a5460b4ea329222d4
Lucas Soares <lucas.alexandre-soares@kolibrigames.com>
2022-03-26 02:25:57 -0300

    Make a global object to dispatch coroutine from non-monobehaviour objects

---
+++ Assets/Scripts/GlobalCoroutineDispatcher/GlobalCoroutineDispatcher.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+
+public class GlobalCoroutineDispatcher : MonoBehaviour, IGlobalCoroutineDispatcher {
+
+	public Coroutine Dispatch(IEnumerator coroutine)
+	{
+		return StartCoroutine(coroutine);
+	}
+}
---
+++ Assets/Scripts/GlobalCoroutineDispatcher/GlobalCoroutineDispatcherInstaller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName="CoroutineDispatcherInstaller", menuName="Installers/CoroutineDispatcherInstaller")]
+public class GlobalCoroutineDispatcherInstaller : ScriptableObjectInstaller<GlobalCoroutineDispatcherInstaller>
+{
+	public GameObject coroutineObjectPrefab;
+
+	public override void InstallBindings()
+	{
+		Container.BindInterfacesAndSelfTo<GlobalCoroutineDispatcher>()
+			.FromComponentInNewPrefab(coroutineObjectPrefab)
+			.AsSingle(); 
+	}
+}
