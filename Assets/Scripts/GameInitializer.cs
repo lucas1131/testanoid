b3bc630c7f639689f7390ec8bc0efe228198e9a2
Lucas Soares <lucas.alexandre-soares@kolibrigames.com>
2022-03-26 17:41:35 -0300

    I was being dumb, revert removal of game initializer

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInitializer : MonoBehaviour
+{
+	[SerializeField] private GameConfig _gameConfig;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        	StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+    	SceneManager.LoadScene("Gameplay");
+    }
+}
