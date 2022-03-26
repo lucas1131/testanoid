948a86eef4ad0da5429849f86df80abedcbdb575
Lucas Soares <lucas.alexandre-soares@kolibrigames.com>
2022-03-26 18:33:30 -0300

    Fix inconsistent bracket

--- Assets/Scripts/Ball/BallPresenter.cs
+++ Assets/Scripts/Ball/BallPresenter.cs
@@ -6,7 +6,8 @@ public class BallPresenter : MonoBehaviour, IBallRigidbodyGetter, IBallPositione
     [SerializeField] private Rigidbody2D _ball;
     public Rigidbody2D Ball => _ball;
 
-    public Vector3 Position {
+    public Vector3 Position 
+    {
         get => gameObject.transform.position;
         set => gameObject.transform.position = value;
     }
